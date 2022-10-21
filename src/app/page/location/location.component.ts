0d2c51214fbd3a1ab3813d97f37e740b441815f7
cannt <nguyenthiencan94@gmail.com>
2022-10-21 15:29:37 +0700

    Update map location

--- src/app/page/location/location.component.ts
+++ src/app/page/location/location.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {catchError, map, Observable, of} from "rxjs";
-import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-location',
@@ -8,29 +6,16 @@ import {HttpClient} from "@angular/common/http";
   styleUrls: ['./location.component.css']
 })
 export class LocationComponent implements OnInit {
-  apiLoaded: Observable<boolean>;
   options: google.maps.MapOptions = {
     center: {lat: 20.964623958230966, lng: 105.82886138441283},
-    zoom: 18
+    zoom: 17,
   };
 
-  apiLoadedGroom: Observable<boolean>;
   optionsGroom: google.maps.MapOptions = {
-    center: {lat: 20.7969722, lng: 105.8869806},
-    zoom: 18
+    center: {lat: 20.796947124649304, lng: 105.8875251176399},
+    zoom: 17
   };
-  constructor(httpClient: HttpClient) {
-    this.apiLoaded = httpClient.jsonp('https://maps.googleapis.com/maps/api/js?key=AIzaSyAwFnSt3lAmOHQL8FZ2Tw4byoTEv4Gt15s&libraries=places&sensor=false', 'callback')
-      .pipe(
-        map(() => true),
-        catchError(() => of(false)),
-      );
-
-    this.apiLoadedGroom = httpClient.jsonp('https://maps.googleapis.com/maps/api/js?key=AIzaSyAwFnSt3lAmOHQL8FZ2Tw4byoTEv4Gt15s&libraries=places&sensor=false', 'callback')
-      .pipe(
-        map(() => true),
-        catchError(() => of(false)),
-      );
+  constructor() {
   }
 
   ngOnInit(): void {
