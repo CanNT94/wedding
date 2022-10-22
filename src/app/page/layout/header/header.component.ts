585a5c11f03ff2d225ba88f1e78ed7163a1b59e2
cannt <nguyenthiencan94@gmail.com>
2022-10-23 01:28:53 +0700

    Update auto x button

--- src/app/page/layout/header/header.component.ts
+++ src/app/page/layout/header/header.component.ts
@@ -1,16 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {Subject, takeUntil} from "rxjs";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
 
   expand = false;
-  constructor() { }
+  $destroy = new Subject<void>();
+  constructor(private router: Router) {
+    router.events.pipe(takeUntil(this.$destroy)).subscribe(() => {
+      this.expand = false;
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+    this.$destroy.next();
+    this.$destroy.complete();
   }
 
 }
