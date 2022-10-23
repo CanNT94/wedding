a9a63506786abe632499d38ff7a5d7d7ce26b849
cannt <nguyenthiencan94@gmail.com>
2022-10-23 21:04:27 +0700

    Update image

---
+++ src/app/app-constant.ts
@@ -0,0 +1,26 @@
+export const images = [
+  {
+    name: '1',
+    src: 'assets/slide/1.jpg'
+  },
+  {
+    name: '2',
+    src: 'assets/slide/2.jpg'
+  },
+  {
+    name: '3',
+    src: 'assets/slide/3.jpg'
+  },
+  {
+    name: '4',
+    src: 'assets/slide/4.jpg'
+  },
+  {
+    name: '5',
+    src: 'assets/slide/5.jpg'
+  },
+  {
+    name: '6',
+    src: 'assets/slide/6.jpg'
+  },
+];
--- src/app/page/photo-library/photo-library.component.ts
+++ src/app/page/photo-library/photo-library.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import { images } from 'src/app/app-constant';
 // import Swiper core and required modules
 import SwiperCore, {Autoplay, FreeMode, Navigation, Pagination, Thumbs, Zoom} from "swiper";
 
@@ -11,32 +12,7 @@ SwiperCore.use([FreeMode, Navigation, Thumbs, Autoplay, Pagination, Zoom]);
 })
 export class PhotoLibraryComponent implements OnInit {
   thumbsSwiper: any;
-  images = [
-    {
-      name: '1',
-      src: 'assets/slide/1.jpg'
-    },
-    {
-      name: '2',
-      src: 'assets/slide/2.jpg'
-    },
-    {
-      name: '3',
-      src: 'assets/slide/3.jpg'
-    },
-    {
-      name: '4',
-      src: 'assets/slide/4.jpg'
-    },
-    {
-      name: '5',
-      src: 'assets/slide/5.jpg'
-    },
-    {
-      name: '6',
-      src: 'assets/slide/6.jpg'
-    },
-  ];
+  images = images;
   constructor() { }
 
   ngOnInit(): void {
